6ed1763ff10da361982e7da07545b3f80f7ea441
Matthew Orlando <maorlando@gmail.com>
2016-01-23 13:27:08 -0800

    Better way to handle Script.fsx changes

--- project/Script.fsx
+++ project/Script.fsx
@@ -1,5 +1,10 @@
 ﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
+// This is just a scratch pad for REPL-driven development. You should run
+// the following command from the git root to prevent pollution:
+//
+//   git update-index --assume-unchanged project/Script.fsx
+
 #load "../src/Client.fs"
 
