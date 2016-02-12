4669ee0e65f762d3f3403adc7fe7329d4c65d816
Matthew Orlando <cogwheel@offbaseproductions.com>
2016-02-12 08:57:48 -0800

    Adding release versions of load project scripts

---
+++ project/Scripts/load-references-release.fsx
@@ -0,0 +1,5 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#r "../packages/Microsoft.Net.Http.2.2.29/lib/portable-net40+sl4+win8+wp71+wpa81/System.Net.Http.dll"
+#r "../packages/Microsoft.Net.Http.2.2.29/lib/portable-net40+sl4+win8+wp71+wpa81/System.Net.Http.Extensions.dll"
+#r "../packages/Microsoft.Net.Http.2.2.29/lib/portable-net40+sl4+win8+wp71+wpa81/System.Net.Http.Primitives.dll"
