a08ed097b4ba9f4b7428ff97a4cb1191ba90657c
Matthew Orlando <maorlando@gmail.com>
2016-03-13 13:32:00 -0700

    Formatting and other minor cleanup

--- src/CaseSet.fs
+++ src/CaseSet.fs
@@ -25,15 +25,15 @@ open pclytics.Reflection
    case tag to union value *)
 
 type CaseSet<'a when 'a : comparison> private (theMap : Map<int, 'a>) =
-    new(theSeq : 'a seq) =
+    new (theSeq : 'a seq) =
         (* Wish I could express this as a constraint, but it fails early
            enough for my purposes *)
         if not (FSharpType.IsUnion typeof<'a>) then
             failwith "CaseSet value type must be a discriminated union"
 
-        CaseSet(theSeq
-                |> Seq.map (fun a -> getCaseTag a, a)
-                |> Map.ofSeq)
+        CaseSet (theSeq
+                 |> Seq.map (fun a -> getCaseTag a, a)
+                 |> Map.ofSeq)
 
     member private x.InnerMap = theMap
 
@@ -42,7 +42,7 @@ type CaseSet<'a when 'a : comparison> private (theMap : Map<int, 'a>) =
         |> CaseSet
 
     member x.Contains (item : 'a) =
-        theMap.ContainsKey(getCaseTag item)
+        theMap.ContainsKey (getCaseTag item)
 
     member x.Count = theMap.Count
 
@@ -51,39 +51,37 @@ type CaseSet<'a when 'a : comparison> private (theMap : Map<int, 'a>) =
         |> CaseSet
 
     override x.Equals other =
-        x.InnerMap = (other :?> 'a CaseSet).InnerMap
+        theMap = (other :?> 'a CaseSet).InnerMap
 
-    override x.GetHashCode () =
-        x.InnerMap.GetHashCode()
+    override x.GetHashCode () = theMap.GetHashCode ()
 
     interface IComparable with
         member x.CompareTo other =
             (theMap :> IComparable).CompareTo (other :?> 'a CaseSet).InnerMap
 
     interface ICollection<'a> with
-        member x.Contains(item) =
-            x.Contains(item)
+        member x.Contains item = x.Contains item
 
-        member x.CopyTo(array, arrayIndex) =
+        member x.Count = x.Count
+
+        member x.GetEnumerator () : IEnumerator<'a> = 
+            (theMap
+             |> Seq.map (fun (KeyValue(_, v)) -> v)
+             :> IEnumerable<'a>)
+             .GetEnumerator ()
+
+        member x.GetEnumerator () : IEnumerator = 
+            (x
+             :> IEnumerable<'a>)
+             .GetEnumerator ()
+             :> IEnumerator
+
+        member x.CopyTo (array, arrayIndex) =
             theMap
             |> Map.fold (fun i _ v -> array.[i] <- v ; i + 1) arrayIndex
             |> ignore
 
-        member x.Count = x.Count
-
-        member x.GetEnumerator(): IEnumerator<'a> = 
-            (theMap
-             |> Seq.map (fun (KeyValue(_, v)) -> v)
-             :> IEnumerable<'a>)
-             .GetEnumerator()
-
-        member x.GetEnumerator(): IEnumerator = 
-            (x
-             :> IEnumerable<'a>)
-             .GetEnumerator()
-             :> IEnumerator
-
         member x.IsReadOnly = true
-        member x.Add(item) = raise (NotSupportedException("ReadOnlyCollection"))
-        member x.Remove(item) = raise (NotSupportedException("ReadOnlyCollection"))
-        member x.Clear() = raise (NotSupportedException("ReadOnlyCollection"))
+        member x.Add _ = raise (NotSupportedException "ReadOnlyCollection")
+        member x.Remove _ = raise (NotSupportedException "ReadOnlyCollection")
+        member x.Clear () = raise (NotSupportedException "ReadOnlyCollection")
